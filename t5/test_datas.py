741e1275f8f4c8579cc21e86b450f9cdc3ef5260
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-05-25 17:14:50 +0300

    Update test_datas.py

--- t5/test_datas.py
+++ t5/test_datas.py
@@ -1,6 +1,7 @@
- def load_data(self, data_file):   
-	with open(data_file, 'r', encoding='utf-8') as f:
+def load_data(data_file):   
+    with open(data_file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
+    total = 0
     for line in lines:
         line = line.strip()
         if not line:  # boş satır kontrolü
@@ -8,6 +9,8 @@
         if line.startswith("//") or " -> " not in line:
             continue  # yorum satırı veya geçersiz satır atla
         input_text, output_text = line.split(" -> ", 1)
-		print(input_text,output_text); 
-
+        total += 1
+        print(input_text, output_text)
+    print("total: ",total)
+# Fonksiyon çağrısı
 load_data("training_data.txt")
